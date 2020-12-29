255e54320fac9e160383b245dbc5dd26e498c86b
Juan Arturo Flores Rivera <ing.flores.rivera@gmail.com>
2020-12-28 18:35:58 -0600

    Agregamos el archivo Py

--- BuscarIP.py
+++ BuscarIP.py
@@ -4,4 +4,4 @@ hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 
 print("\nEl nombre de tu computadora es: " + hostname)
-print("Tu dirección IP es: " + ip + "\n")
+print("\nTu dirección IP es: " + ip + "\n")
